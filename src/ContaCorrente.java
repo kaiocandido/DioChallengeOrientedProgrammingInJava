a03c512baddbea432c79e8acfc03ca2d8e64de5a
adm <kaio.candf@gmail.com>
2025-07-31 21:31:33 -0300

    primeiro commit

---
+++ src/ContaCorrente.java
@@ -0,0 +1,29 @@
+public class ContaCorrente {
+    private int agencia;
+    private int numero;
+    private double saldo;
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void sacar(){
+
+    }
+
+    public void transferir(){
+
+    }
+
+    public void depositar(){
+
+    }
+}
